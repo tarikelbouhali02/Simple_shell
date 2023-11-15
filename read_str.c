8b9a1e4b5cc77a1d9379f46b8144d1411bc3ec1c
tarikelbouhali02 <tarikbh99@gmail.com>
2023-11-15 13:08:25 -0800

    g

---
+++ read_str.c
@@ -0,0 +1,50 @@
+//rread_stream.c
+#include "shell.h"
+
+/**
+ * rread_stream - read a line from the stream
+ *
+ * Return: pointer that points the the read line
+ */
+char *rread_stream(void)
+{
+ int bufsize = 1024;
+ int i = 0;
+ char *line = malloc(sizeof(char) * bufsize);
+ int character;
+
+if (line == NULL)
+ {
+  fprintf(stderr, "allocation error in rread_stream");
+  exit(EXIT_FAILURE);
+ }
+ while (1)
+ {
+  character = getchar(); /* read first char from stream */
+  if (character == EOF)
+  {
+   free(line);
+   exit(EXIT_SUCCESS);
+  }
+  else if (character == '\n')
+  {
+   line[i] = '\0';
+   return (line);
+  }
+  else
+  {
+   line[i] = character;
+  }
+  i++;
+  if (i >= bufsize)
+  {
+   bufsize += bufsize;
+   line = realloc(line, bufsize);
+   if (line == NULL)
+   {
+    fprintf(stderr, "reallocation error in rread_stream");
+    exit(EXIT_FAILURE);
+   }
+  }
+ }
+}
