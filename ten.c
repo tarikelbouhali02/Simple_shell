487e68eb516d3d93e9aea80cc9081bb2c124deca
almasike <almasike000@gmail.com>
2023-11-17 10:43:21 -0800

    correction simple_shell

--- ten.c
+++ ten.c
@@ -134,12 +134,14 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	return (0);
 }
 
+
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
  *
  * Return: void
  */
+
 void free_list(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
