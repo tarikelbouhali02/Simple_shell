db3982eda63ef4ef2213326ddefc35a1e4fca568
tarikelbouhali02 <tarikbh99@gmail.com>
2023-11-17 13:55:07 -0800

    g

--- main.c
+++ main.c
@@ -1,44 +1,97 @@
-#include "shell.h"
+#include "simpleshell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
+ * main - entry point of our shell
+ * @argc: the number of arguments to main
+ * @argv: the list of arguments
+ * @env: the environement
+ * Return: always 0
  */
-int main(int ac, char **av)
+
+int main(int argc, char **argv, char **env)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	data_t *data;
+	int fd;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
+	data = sh_data_new(argv, env);
+	if (argc > 1)
 	{
-		fd = open(av[1], O_RDONLY);
+		data->mode = FROMFILE;
+		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+			_puts(data->bash);
+			_puts(": 0: Can't open ");
+			_puts(argv[1]);
+			_puts("\n");
+			sh_data_delete(data);
+			return (127);
 		}
-		info->readfd = fd;
+		sh_start(data, fd);
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	else
+	{
+		if (isatty(STDIN_FILENO))
+			data->mode = INTERACTIVE;
+		else
+			data->mode = NONINTERACTIVE;
+		sh_start(data, STDIN_FILENO);
+	}
+	sh_data_delete(data);
 	return (EXIT_SUCCESS);
 }
+
+/**
+ * stop_signal_handler - the command to execute on Ctrl+C
+ *
+ * @signum: the value of the signal
+ */
+
+void stop_signal_handler(int signum)
+{
+	(void)signum;
+	_puts("\n$ > ");
+}
+
+/**
+ * sh_start - when the type of execution has been choosen, parses and executes
+ * commands
+ *
+ * @data: the data_t structure
+ * @fd: the file descriptor to read
+ *
+ * Return: EXIT_SUCCESS always because all errors should be treated
+ */
+
+int sh_start(data_t *data, int fd)
+{
+	cmd_lst_lst_t	*head = NULL;
+	char		*line = NULL;
+	int		ret = 0;
+
+	if (data->mode == INTERACTIVE)
+		_puts("$ > ");
+
+	data->line = &line;
+	signal(SIGINT, stop_signal_handler);
+	while ((ret = sh_getline(&line, fd)) != EOF)
+	{
+		if (parser(line, &head) != -1)
+		{
+			data->llav_head = head;
+			expansion(data, &head);
+			execute(data, &head);
+			free_all(&head);
+		}
+		data->lines++;
+		free(line);
+		if (data->mode == INTERACTIVE)
+			_puts("$ > ");
+	}
+	if (line)
+		free(line);
+	if (data->mode == FROMFILE)
+		close(fd);
+	return (EXIT_SUCCESS);
+}
+
